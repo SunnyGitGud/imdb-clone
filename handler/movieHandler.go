2e44cd4f642807ac26e9bae7ad4255c52572fbdb
SunnyGitGud <wecandothissunny@gmail.com>
2025-11-13 22:36:49 +0530

    get movie by id working

--- handler/movieHandler.go
+++ handler/movieHandler.go
@@ -3,8 +3,8 @@ package handler
 import (
 	"encoding/json"
 	"net/http"
+	"strconv"
 
-	"github.com/gorilla/mux"
 	"github.com/sunnygitgud/imdb-clone/db/gen"
 	"github.com/sunnygitgud/imdb-clone/logger"
 )
@@ -19,6 +19,17 @@ func (h *MovieHandler) writeJsonResponse(w http.ResponseWriter, data any) {
 	if err := json.NewEncoder(w).Encode(data); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
+}
+
+func (h *MovieHandler) parseID(w http.ResponseWriter, idStr string) (int, bool) {
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		h.Log.Error("Invalid Id Format", err)
+		http.Error(w, "Invalid ID", http.StatusBadRequest)
+		return 0, false
+	}
+	return id, true
+
 }
 
 func (h *MovieHandler) GetTopMovies(w http.ResponseWriter, r *http.Request) {
@@ -45,18 +56,44 @@ func (h *MovieHandler) GetRandomMovies(w http.ResponseWriter, r *http.Request) {
 
 func (h *MovieHandler) GetMovieById(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
-	movies, err := h.Repo.GetMoviesById(ctx, 3318)
+	idStr := r.URL.Path[len("/api/movies/"):]
+	id, ok := h.parseID(w, idStr)
+	if !ok {
+		return
+	}
+	movies, err := h.Repo.GetMoviesById(ctx, int32(id))
+	if err != nil {
+		h.Log.Error("failed to get movies:", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	h.writeJsonResponse(w, movies)
+}
+
+func (h *MovieHandler) SearchMovies(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	query := r.URL.Query().Get("q")
+	order := r.URL.Query().Get("order")
+	genreStr := r.URL.Query().Get("genre")
+	genreID := 0
+
+	if genreStr != "" {
+		if g, err := strconv.Atoi(genreStr); err != nil {
+			genreID = g
+		}
+	}
+	var limit int32 = 10
+	param := db.SearchMoviesParams{
+		SearchTerm:  query,
+		OrderBy:     order,
+		GenreID:     int32(genreID),
+		ResultLimit: limit,
+	}
+	movies, err := h.Repo.SearchMovies(ctx, param)
 	if err != nil {
 		h.Log.Error("failed to get movies:", err)
 		http.Error(w, "Failed to fetch top movies", http.StatusInternalServerError)
 		return
 	}
 	h.writeJsonResponse(w, movies)
-}
-
-func (h *MovieHandler) SearchMovie(w http.ResponseWriter, r *http.Request) {
-	ctx := r.Context()
-	movieId := mux.Vars(r)
-	Id := movieId["id"]
-	movies, err := h.Repo.SearchMovies(ctx, Id)
 }
